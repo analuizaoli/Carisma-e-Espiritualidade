e7514e0dcdc9ed6e6c927768ecf2d38c7b0e3903
analuizaoli <anaiza.0502@gmail.com>
2024-11-28 20:25:07 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Fraternidade Belém',
+    '“COMO” os pobres',
+    'Irmãos que vivem em proximidade radical com os mais pobres, inseridos nos bolsões de pobreza, tornando-se "COMO" os pobres.'
+)
+
+criaCartao(
+    'Fraternidade Nazaré',
+    '“COM” os pobres',
+    'Fraternidade que convive diretamente "COM" os “filhos da aliança” (ex-moradores de rua, órfãos, ex-presidiários, entre outros), resgatando-os com a mesma paixão do Bom Pastor.'
+)
+
+criaCartao(
+    'Fraternidade Cenáculo',
+    '“PARA” os pobres',
+    'Irmãos que vivem a pobreza com sobriedade, dedicados ao anúncio do evangelho e ao trabalho "PARA" os pobres. As casas Cenáculo unem ricos e pobres em fraternidade, promovendo comunhão e um novo Pentecostes de Misericórdia.'
+) 
+
+criaCartao(
+    'Sobre',
+    'Carisma e Espiritualidade',
+    'A Aliança de Misericórdia vive a espiritualidade de Maria, Palavra, Espírito e Pobres, centrada no amor misericordioso de Jesus. Sua missão é manifestar o amor de Deus, promovendo a dignidade dos pobres por meio de ações concretas, vivendo "Como", "Com" e "Para" eles.'
+)
